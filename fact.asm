dce5bfac2110c6a7fb80398b8365d5b87ee3f7ad
tasakos-dev <59792112+tasakos-dev@users.noreply.github.com>
2020-08-13 23:41:36 +0300

    Add user Interaction

--- fact.asm
+++ fact.asm
@@ -1,13 +1,43 @@
+## Factorial in MIPS assembly
+# by tasakos
+
+		
 	.globl main
+	.data	
+		inputMessage: .asciiz "Enter an integer:\n"
+		message1: .asciiz "The factorial of number "
+		message2: .asciiz " is "
 	
 	
 	.text
 main:
-	addi $a0 ,$0, 3
-	jal factorial
-	add $a0,$v0,$0
+	#read integer
+	la $a0, inputMessage
+	li $v0, 4
+	syscall	
+	li $v0,5
+	syscall
+	add $t0,$v0,$0
+	#Print the message1
+	la $a0, message1
+	li $v0 ,4
+	syscall
+	#Print the argument of factorial
+	add $a0 ,$0, $t0
 	li $v0 1
 	syscall
+	#Call procedure Factorial
+	jal factorial
+	#Keep the result of factorial to use later
+	add $t1,$v0,$0
+	#Print the message1
+	la $a0, message2
+	li $v0 ,4
+	syscall 
+	add $a0,$t1,$0
+	li $v0 1
+	syscall
+	#Tell to OS that the Program ended
 	li $v0, 10
 	syscall
 
@@ -29,12 +59,3 @@ Else:
 	addi $sp,$sp,8
 	mul $v0,$a0,$v0
 	jr $ra
-
-	
-
-		
-		
-
-		
-	
-	
